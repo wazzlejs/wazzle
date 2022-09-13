c4f065c163ca76b4fe023060362b53051354b6d7
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2022-09-13 17:07:26 +0200

    chore: more types ideas

--- packages/dazzle/src/types/global.ts
+++ packages/dazzle/src/types/global.ts
@@ -1,6 +1,13 @@
 declare global {
     export namespace RazzleTypes {
         export interface RazzleConfig {
+            modifyWebpack5Options: (options: RazzleTypes.RazzleWebpack5Options) => RazzleTypes.RazzleWebpack5Options;
+            modifyWebpack5Config: (config: ̧unknown, options: RazzleTypes.RazzleWebpack5Options) => RazzleTypes.RazzleWebpack5Options;
+        }
+        export interface RazzleContext {
+            
+        }
+        export interface RazzleWebpack5Options {
             
         }
     }
