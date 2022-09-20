080aad74eacaaddaff1f1556eb4d859ebec49107
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2022-09-20 14:22:31 +0200

    chore: add more types

--- packages/dazzle-plugin-webpack5/src/types/global.ts
+++ packages/dazzle-plugin-webpack5/src/types/global.ts
@@ -1,5 +1,8 @@
 import { Webpack5Plugin } from "./types";
 
+import { Configuration } from "webpack";
+import { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
 declare global {
     export namespace DazzleTypes {
         export interface Plugins {
@@ -8,9 +11,16 @@ declare global {
 
         export interface Config {
             modifyWebpack5Context: (webpack5Context: DazzleTypes.Webpack5Context, DazzleContext: DazzleTypes.Context) => DazzleTypes.Webpack5Context;
-            modifyWebpack5Config: (config: unknown, webpack5Context: DazzleTypes.Webpack5Context) => DazzleTypes.Webpack5Context;
+            modifyWebpack5Config: (config: Configuration, webpack5Context: DazzleTypes.Webpack5Context) => DazzleTypes.Webpack5Context;
         }
         export interface Context {
+            devMatrixName: string;
+            buildMatrix: Record<string, DazzleTypes.BuildConfig>;
+        }
+
+        export interface BuildConfig {
+            targets: Array<string>;
+            depends?: Record<string, Array<string> | string>;
         }
 
         export interface Webpack5Plugins {
@@ -19,6 +29,15 @@ declare global {
         export interface Webpack5Context {
             plugin: Webpack5Plugin;
             plugins: Webpack5Plugins;
+            matrixName: string;
+            buildTargets: Array<string>;
+            buildTarget: string;
+            buildName: string;
+            isDevEnv: boolean;
+            isDev: boolean;
+            isProd: boolean;
+            isClient: boolean;
+            isServer: boolean
         }
     }
 }
