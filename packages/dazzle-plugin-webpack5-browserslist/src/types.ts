2de6e9a1bb24a5e77591e5f8a87a43f132422eaf
Daniel Tabuenca <dtabuenc@gmail.com>
2022-10-01 08:01:51 -0500

    Add browserslist plugin

---
+++ packages/dazzle-plugin-webpack5-browserslist/src/types.ts
@@ -0,0 +1,8 @@
+import {} from '@elzzad/dazzle';
+import {} from '@elzzad/dazzle-plugin-webpack5';
+
+declare module '@elzzad/dazzle/types' {
+  export interface DazzleContext {
+    browserslistEnvs: string[];
+  }
+}
