2505f404211ab4829645f553b37f1970243a0daa
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2022-10-19 18:39:29 +0200

    Use overrides

--- examples/basic-webpack5/test.cjs
+++ examples/basic-webpack5/test.cjs
@@ -1,10 +1,9 @@
 (async function () {
     try {
-        const foo = await import('./dazzle.config.ts')
+        const foo = require('./dazzle.config.ts')
         console.log(foo);
     } catch (error) {
         console.log(error);
     }
 })()
 
-export default {}
