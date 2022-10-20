fea29196ff8c4a85232ec0f5bbe210094533bb24
Daniel Tabuenca <dtabuenc@gmail.com>
2022-10-20 00:22:41 -0500

    Use rechoir instead of registering loader in node opts

--- examples/basic-webpack5/test.cjs
+++ examples/basic-webpack5/test.cjs
@@ -1,3 +1,7 @@
+const { prepare } = require('rechoir');
+const interpret  = require('interpret');
+prepare(interpret.extensions, './dazzle.config.ts');
+
 (async function () {
     try {
         const foo = require('./dazzle.config.ts')
